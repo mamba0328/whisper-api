68dcec6ad539b5df86c6837ecf3248274b20d7a4
valentindj <valentindj@starway-media.com>
2024-02-18 12:57:39 +0200

    app error handler refactor

--- app.ts
+++ app.ts
@@ -31,15 +31,7 @@ app.use(function (req:Request, res:Response, next:CallableFunction) {
 
 // error handler
 app.use(function (err:Error, req:Request, res:Response) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development"
-        ? err
-        : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
+    res.status(err.status || 500).json(err);
 });
 
 app.use(disconnectFromMongoDB);
