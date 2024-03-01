cd49faf98f0f69c8400a0df647d0748af749cc73
valentindj <valentindj@starway-media.com>
2024-03-01 17:41:48 +0200

    userContacts tests and validation refactored to support authentication

--- middleware/validation/userContactsValidators.ts
+++ middleware/validation/userContactsValidators.ts
@@ -1,27 +1,31 @@
 import { body, param, query } from "express-validator";
 import { checkEntityExistsInDataBaseById } from "../../helpers/checkEntityExistsInDB";
-import { Error } from "../../types/types";
+import { Error, User } from "../../types/types";
 import { UserContacts } from "../../models/UserContacts";
 import { Users } from "../../models/Users";
+import { Types } from "mongoose";
 
 export const getValidators = [
     query("skip").isNumeric().optional(),
     query("limit").isNumeric().optional(),
     query("username").isString().optional(),
-    query("contact").isString().optional()
+    query("contact").isString().optional(),
+    query("username").custom((username:string, { req }) => usernameInRequestMatchesAuthUser(username, req.user as User))
 ];
 export const postValidators = [
-    body("user_id").isMongoId().custom(async (id:string) => await checkEntityExistsInDataBaseById(id, Users)).bail({ level: "request" }),
-    body("contact_id").isMongoId().custom(async (id:string) => await checkEntityExistsInDataBaseById(id, Users)).bail({ level: "request" }),
+    body("user_id").isMongoId().custom(async (id:Types.ObjectId) => await checkEntityExistsInDataBaseById(id, Users)).bail({ level: "request" }),
+    body("contact_id").isMongoId().custom(async (id:Types.ObjectId) => await checkEntityExistsInDataBaseById(id, Users)).bail({ level: "request" }),
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    body("user_id").custom(async (user_id:string, { req }) => await checkContactAlreadyExists(user_id, req.body.contact_id))
+    body("user_id").custom(async (user_id:Types.ObjectId, { req }) => await checkContactAlreadyExists(user_id, req.body.contact_id)),
+    body("user_id").custom((user_id:string, { req }) => userIdInRequestMatchesAuthUser(user_id, req.user as User))
 ];
 export const deleteValidators = [
-    param("id").isMongoId().custom(async (id:string) => await checkEntityExistsInDataBaseById(id, UserContacts))
+    param("id").isMongoId().custom(async (id:Types.ObjectId) => await checkEntityExistsInDataBaseById(id, UserContacts)),
+    param("id").custom(async (id:Types.ObjectId, { req }) => await authUserParticipateInContact(id, req.user as User))
 ];
 
 
-const checkContactAlreadyExists = async (user_id:string, contact_id:string) => {
+const checkContactAlreadyExists = async (user_id:Types.ObjectId, contact_id:Types.ObjectId) => {
     const contactAlreadyExists = await UserContacts.findOne({ user_id, contact_id });
 
     if (contactAlreadyExists) {
@@ -31,3 +35,47 @@ const checkContactAlreadyExists = async (user_id:string, contact_id:string) => {
         throw error;
     }
 };
+
+const usernameInRequestMatchesAuthUser = (username:string, user:User) => {
+    if (user.is_admin) {
+        return true;
+    }
+
+    if (username === user.username) {
+        return true;
+    }
+
+    const error:Error = new Error("You have no right for the action");
+    error.status = 401;
+    throw error;
+};
+
+const userIdInRequestMatchesAuthUser = (user_id:string, user:User) => {
+    if (user.is_admin) {
+        return true;
+    }
+
+    if (user_id === user._id.toString()) {
+        return true;
+    }
+
+    const error:Error = new Error("You have no right for the action");
+    error.status = 401;
+    throw error;
+};
+
+
+const authUserParticipateInContact = async (contact_id:Types.ObjectId, user:User) => {
+    if (user.is_admin) {
+        return true;
+    }
+
+    const contact = await UserContacts.findById(contact_id);
+
+    if (contact!.user_id !== user._id) {
+        const error:Error = new Error("You can't delete someone-else's contact");
+        error.status = 401;
+        throw error;
+    }
+};
+
--- tests/userContacts.test.ts
+++ tests/userContacts.test.ts
@@ -1,54 +1,44 @@
-import request from "supertest";
+// @ts-ignore
+import session from "supertest-session";
 import app from "../app";
 
-import { mockUserTaylor, mockUserLayla } from "./consts/mocks";
+import {mockUserTaylor, mockUserLayla, mockAdmin} from "./consts/mocks";
 import { createUsers } from "./helpers/createUsers";
 import { findAndDeleteInstance } from "./helpers/findAndDeleteInstance";
 
 
 describe("UserContacts API Tests", () => {
+    const testSession = session(app);
+
     const usersIds:string[] = [];
     let contactId:string;
 
     beforeAll( async () => {
-        //create users, so we can then create contact with them
-        const createdUsersIds:string[] = await createUsers([mockUserTaylor, mockUserLayla]);
-        usersIds.push(...createdUsersIds);
+        await testSession.post("/sign-in").send({ identity_field: mockAdmin.username, password: mockAdmin.password });
 
-        //create contact
-        const contactPayload = {user_id: usersIds[0]!, contact_id: usersIds[1]!};
-        const reqUserContact = await request(app).post("/api/user-contacts").send(contactPayload);
-
-        if(reqUserContact.statusCode !== 200){ //usually bad response if contact already exists, so we'll delete it
-            await findAndDeleteInstance("user-contacts", contactPayload)
-            const reqUserContact = await request(app).post("/api/user-contacts").send(contactPayload);
-            return contactId = reqUserContact.body._id;
-        }
-
-        contactId = reqUserContact.body._id;
+        const { body:userResponse } = await testSession.get("/api/users");
+        usersIds.push(userResponse[0]._id, userResponse[1]._id)
     });
 
     afterAll(async () => {
-        if (usersIds.length) {
-            await Promise.all(usersIds.map( async (userId) => {
-                await request(app).del(`/api/users/${userId}`);
-            }))
-        }
-        if(contactId){
-            await request(app).del(`/api/user-contacts/${contactId}`);
-        }
+        await testSession.post("/sign-out").send()
     });
 
     describe("POST /api/user-contacts", () => {
+        it("Creates contact", async() => {
+            const res = await testSession.post("/api/user-contacts").send({user_id: usersIds[0], contact_id: usersIds[1]});
+            contactId = res.body._id;
+            expect(res.statusCode).toBe(200);
+        });
         it("Validation fails for an non-unique contact", async() => {
-            const res = await request(app).post("/api/user-contacts").send({user_id: usersIds[0], contact_id: usersIds[1]});
+            const res = await testSession.post("/api/user-contacts").send({user_id: usersIds[0], contact_id: usersIds[1]});
             expect(res.statusCode).toBe(400);
         });
     });
 
     describe("GET /api/user-contacts", () => {
         it("Returns user contacts", async () => {
-            const getUserResponse = await request(app).get("/api/user-contacts");
+            const getUserResponse = await testSession.get("/api/user-contacts");
             expect(getUserResponse.body.length).toBeGreaterThan(0);
             expect(getUserResponse.body[0].user_id).toEqual(usersIds[0]);
             expect(getUserResponse.body[0].contact_id).toEqual(usersIds[1]);
@@ -57,7 +47,7 @@ describe("UserContacts API Tests", () => {
 
     describe("DEL /api/user-contacts", () => {
         it("Delete user contact", async() => {
-            await request(app).del(`/api/user-contacts/${contactId}`).expect(200);
+            await testSession.delete(`/api/user-contacts/${contactId}`).expect(200);
         })
     })
 })
