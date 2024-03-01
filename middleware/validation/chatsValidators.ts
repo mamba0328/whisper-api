4b17081c3e853e1a30a2e0d5a91450364eb5acbb
valentindj <valentindj@starway-media.com>
2024-03-01 16:39:48 +0200

    chats tests and validation refactored to support authentication

--- middleware/validation/chatsValidators.ts
+++ middleware/validation/chatsValidators.ts
@@ -1,50 +1,52 @@
 import { body, param, query } from "express-validator";
+import { Types } from "mongoose";
+
 import { checkEntityExistsInDataBaseById } from "../../helpers/checkEntityExistsInDB";
 import { Chats } from "../../models/Chats";
 import { Users } from "../../models/Users";
-import { Error } from "../../types/types";
+import { Error, User } from "../../types/types";
 
 export const getValidators = [
     query("skip").isNumeric().optional(),
     query("limit").isNumeric().optional(),
     query("chat_users").isString().optional(),
     query("chat_name").isString().optional(),
-    query("is_group_chat").isString().optional()
+    query("is_group_chat").isString().optional(),
+    query("chat_users").custom((chat_users:string[], { req }) => authenticatedUserAmongChatUsers(chat_users, req.user as User))
 ];
 export const postValidators = [
     body("is_group_chat").optional().isBoolean(),
     body("chat_name").optional().isString().escape(),
     body("chat_users.*").isMongoId().bail({ level: "request" }),
     body("chat_users").if(body("is_group_chat").not().exists()).isArray({ min: 2, max: 2 }),
-    body("chat_users").isArray().custom(async (chat_users) => {
-        if (!chat_users.length) {
-            throw new Error("No empty chat_users");
-        }
-        await Promise.all([chat_users.map(async (id:string) => await checkEntityExistsInDataBaseById(id, Users))]);
-    }).bail({ level: "request" }),
+    body("chat_users").isArray().custom(async (chat_users:Types.ObjectId[]) => allChatUsersAreValid(chat_users)).bail({ level: "request" }),
+    body("chat_users").custom((chat_users:string[], { req }) => authenticatedUserAmongChatUsers(chat_users, req.user as User)),
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    body("chat_users").custom(async (chat_users:string[], { req }) => await checkPersonalChatAlreadyExists(chat_users, req.body.is_group_chat))
+    body("chat_users").custom(async (chat_users:Types.ObjectId[], { req }) => await checkPersonalChatAlreadyExists(chat_users, req.body.is_group_chat))
 
 ];
 export const putValidators = [
-    param("id").isMongoId().custom(async (id:string) => {
-        await checkEntityExistsInDataBaseById(id, Chats);
-        const chat = await Chats.findById(id);
-        if (!chat!.is_group_chat) {
-            throw new Error("Chat is not editable");
-        }
-    }),
+    param("id").isMongoId().custom(async (id:Types.ObjectId) => await checkEntityExistsInDataBaseById(id, Chats)).bail({ level: "request" }),
+    param("id").isMongoId().custom(async (id:Types.ObjectId) => await chatIsEditable(id)).bail({ level: "request" }),
+    param("id").isMongoId().custom(async (chat_id:Types.ObjectId, { req }) => await authUserHasAccessToTheChatByChatId(chat_id, req.user as User)),
     body("chat_name").optional().isString().escape(),
     body("chat_users").optional().isArray().custom(async (chat_users) => {
-        await Promise.all([chat_users.map(async (id:string) => await checkEntityExistsInDataBaseById(id, Users))]);
+        await Promise.all([chat_users.map(async (id:Types.ObjectId) => await checkEntityExistsInDataBaseById(id, Users))]);
     })
 ];
 export const deleteValidators = [
-    param("id").isMongoId().custom(async (id:string) => await checkEntityExistsInDataBaseById(id, Chats))
+    param("id").isMongoId().custom(async (id:Types.ObjectId) => await checkEntityExistsInDataBaseById(id, Chats)).bail({ level: "request" }),
+    param("id").isMongoId().custom(async (chat_id:Types.ObjectId, { req }) => await authUserHasAccessToTheChatByChatId(chat_id, req.user as User))
 ];
 
+const allChatUsersAreValid = async (chat_users:Types.ObjectId[]) => {
+    if (!chat_users.length) {
+        throw new Error("No empty chat_users");
+    }
+    await Promise.all([chat_users.map(async (id:Types.ObjectId) => await checkEntityExistsInDataBaseById(id, Users))]);
+};
 
-const checkPersonalChatAlreadyExists = async (chat_users:string[], is_group_chat:boolean) => {
+const checkPersonalChatAlreadyExists = async (chat_users:Types.ObjectId[], is_group_chat:boolean) => {
     if (is_group_chat) {
         return true;
     }
@@ -53,5 +55,33 @@ const checkPersonalChatAlreadyExists = async (chat_users:string[], is_group_chat
         const error: Error = new Error("Chat already exists");
         error.status = 400;
         throw error;
+    }
+};
+
+const authenticatedUserAmongChatUsers = (chat_users:string[], user:User) => {
+    if (user.is_admin) {
+        return true;
+    }
+
+    if (!chat_users.includes(user._id.toString())) {
+        const error: Error = new Error("User has access only to the chats that he participates in");
+        error.status = 400;
+        throw error;
+    }
+};
+
+const authUserHasAccessToTheChatByChatId = async (chat_id:Types.ObjectId, user:User) => {
+    const chat = await Chats.findById(chat_id);
+
+    const authUserId = user._id;
+    if (!chat!.chat_users.includes(authUserId)) {
+        throw new Error("User has no access to the chat");
+    }
+};
+
+const chatIsEditable = async (chat_id:Types.ObjectId) => {
+    const chat = await Chats.findById(chat_id);
+    if (!chat!.is_group_chat) {
+        throw new Error("Chat is not editable");
     }
 };
--- tests/chats.test.ts
+++ tests/chats.test.ts
@@ -1,57 +1,59 @@
-import request from "supertest";
+// @ts-ignore
+import session from "supertest-session";
 import app from "../app";
+import { mockAdmin } from "./consts/mocks";
+import {User} from "../types/types";
+import {Types} from "mongoose";
 
-import { mockUserTaylor, mockUserLayla, mockUserJohn } from "./consts/mocks";
-import { createUsers } from "./helpers/createUsers";
-import {createChat} from "./helpers/createChat";
 
 describe("Chat API Tests", () => {
-    const usersIds:string[] = [];
-    const personalChatUsers:string[] = [];
+    const testSession = session(app);
 
-    let chatId:string;
-    let groupChatId:string;
+    const usersIds:Types.ObjectId[] = [];
+    const personalChatUsers:Types.ObjectId[] = [];
+
+    let chatId:Types.ObjectId;
+    let groupChatId:Types.ObjectId;
 
     beforeAll( async () => {
-        const createdUsers = await createUsers([mockUserTaylor, mockUserLayla, mockUserJohn]);
+        await testSession.post("/sign-in").send({ identity_field: mockAdmin.username, password: mockAdmin.password });
 
-        usersIds.push(...createdUsers);
-        personalChatUsers.push(...usersIds.slice(0, 2));
+        const { body:usersResponse }:{ body: User[] } = await testSession.get('/api/users/');
 
-        chatId = await createChat(personalChatUsers);
+        usersIds.push(...usersResponse.map(user => user._id));
+        personalChatUsers.push(usersResponse[0]!._id, usersResponse[1]!._id);
     });
 
     afterAll(async () => {
-        if (usersIds.length) {
-            await Promise.all(usersIds.map( async (userId) => {
-                await request(app).del(`/api/users/${userId}`);
-            }))
-        }
-        if(chatId){
-            await request(app).del(`/api/chats/${chatId}`);
-        }
+        await testSession.post("/sign-out").send()
     });
 
     describe("POST /api/chats", () => {
-        it("Validation fails for an non-unique chat", async() => {
-            const res = await request(app).post("/api/chats").send({chat_users: personalChatUsers});
-            expect(res.statusCode).toBe(400);
-        });
         it("Validation fails for a personal chat with more then 2 users", async() => {
-            const res = await request(app).post("/api/chats").send({chat_users: [...usersIds]});
+            const res = await testSession.post("/api/chats").send({chat_users: [...usersIds]});
             expect(res.statusCode).toBe(400);
         });
         it("Validation fails for a personal chat with less then 2 users", async() => {
-            const res = await request(app).post("/api/chats").send({chat_users: [usersIds[0]]});
+            const res = await testSession.post("/api/chats").send({chat_users: [usersIds[0]]});
             expect(res.statusCode).toBe(400);
         });
         it("Validation doesnt allow empty chats", async() => {
-            const res = await request(app).post("/api/chats").send({chat_users: []});
+            const res = await testSession.post("/api/chats").send({chat_users: []});
+            expect(res.statusCode).toBe(400);
+        });
+        it("Creates group-chat with the same users", async() => {
+            const chatPayload = { chat_users: personalChatUsers }
+            const res = await testSession.post("/api/chats").send(chatPayload);
+            chatId = res.body._id;
+            expect(res.statusCode).toBe(200);
+        })
+        it("Validation fails for an non-unique chat", async() => {
+            const res = await testSession.post("/api/chats").send({chat_users: personalChatUsers});
             expect(res.statusCode).toBe(400);
         });
         it("Creates group-chat with the same users", async() => {
             const chatPayload = { chat_users: usersIds, chat_name: 'unique_name', is_group_chat: true }
-            const res = await request(app).post("/api/chats").send(chatPayload);
+            const res = await testSession.post("/api/chats").send(chatPayload);
             groupChatId = res.body._id;
             expect(res.statusCode).toBe(200);
         })
@@ -59,11 +61,11 @@ describe("Chat API Tests", () => {
 
     describe("PUT /api/chats", () => {
         it("Validation fails for a personal chat", async() => {
-            const res = await request(app).put(`/api/chats/${chatId}`).send({chat_users: usersIds});
+            const res = await testSession.put(`/api/chats/${chatId}`).send({chat_users: usersIds});
             expect(res.statusCode).toBe(400);
         });
         it("Changes applied to the groupchat", async() => {
-            const res = await request(app).put(`/api/chats/${groupChatId}`).send({chat_users: personalChatUsers, chat_name:'new_name'},);
+            const res = await testSession.put(`/api/chats/${groupChatId}`).send({chat_users: personalChatUsers, chat_name:'new_name'},);
             expect(res.statusCode).toBe(200);
             expect(res.body.chat_name).toEqual("new_name");
             expect(res.body.chat_users).toEqual(personalChatUsers);
@@ -72,13 +74,13 @@ describe("Chat API Tests", () => {
 
     describe("DEL /api/chats", () => {
         it("Deletes chat", async() => {
-            await request(app).del(`/api/chats/${chatId}`).expect(200);
+            await testSession.delete(`/api/chats/${chatId}`).expect(200);
         })
         it("Deletes group chat", async() => {
-            await request(app).del(`/api/chats/${groupChatId}`).expect(200);
+            await testSession.delete(`/api/chats/${groupChatId}`).expect(200);
         })
         // it("No chat messages left", async() => {
-        //     await request(app).del(`/api/user-contacts/${contactId}`).expect(200);
+        //     await testSession.del(`/api/user-contacts/${contactId}`).expect(200);
         // })
     })
 })
