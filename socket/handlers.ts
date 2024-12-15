a030ab23732fd4e3d1bc9e7bbe4834a22b572592
valentindj <valentindj@starway-media.com>
2024-12-15 19:11:55 +0200

    new socket message events added

--- socket/handlers.ts
+++ socket/handlers.ts
@@ -5,11 +5,24 @@ import { Chats } from "../models/Chats";
 import { MessageSeenBy } from "../models/MessageSeenBy";
 
 export default (io:Server, socket:Socket) => {
-    socket.on("message", async (message:Message, callback:CallableFunction) => {
+    socket.on("enterRoom", async (chatId:string) => {
+        if (!chatId) {
+            return;
+        }
+        await socket.join(chatId);
+    });
+
+    socket.on("leaveRoom", async (chatId:string) => {
+        if (!chatId) {
+            return;
+        }
+        await socket.leave(chatId);
+    });
+
+    socket.on("createMessage", async (message:Message, callback:CallableFunction) => {
         // @ts-ignore
         const sender = socket.request.user;
         const chat = await Chats.findById(message.chat_id);
-
         const receivers = chat?.chat_users.filter((user_id) => user_id.toString() !== sender._id.toString()) ?? [];
 
         const newMessage = await ChatMessages.create({
@@ -18,7 +31,8 @@ export default (io:Server, socket:Socket) => {
             created_at: new Date().toISOString() // now
         });
 
-        socket.to(message.chat_id.toString()).emit("message", newMessage);
+        socket.to(message.chat_id.toString()).emit("newMessage", newMessage);
+
 
         receivers.forEach((receiverId) => {
             const receiverPersonalRoom = io.sockets.adapter.rooms.get(receiverId.toString());
@@ -39,6 +53,36 @@ export default (io:Server, socket:Socket) => {
         callback(newMessage);
     });
 
+    socket.on("deleteMessage", async (message:Message, callback:CallableFunction) => {
+        // @ts-ignore
+        const wasDeleted = await ChatMessages.findByIdAndDelete(message._id);
+
+        if (!wasDeleted) {
+            callback(false);
+            return;
+        }
+
+        socket.to(message.chat_id.toString()).emit("messageWasDeleted", message._id);
+
+        callback(true);
+    });
+
+    socket.on("updateMessage", async (message:Message, callback:CallableFunction) => {
+        // @ts-ignore
+        const wasUpdated = await ChatMessages.findByIdAndUpdate(message._id, { body: message.body, updated_at: new Date().toISOString() });
+
+        if (!wasUpdated) {
+            callback(null);
+            return;
+        }
+
+        const updatedMessage = await ChatMessages.findById(message._id);
+
+        socket.to(message.chat_id.toString()).emit("messageWasUpdated", updatedMessage);
+
+        callback(updatedMessage);
+    });
+
     socket.on("seenMessage", async ({ user_id, message_id, chat_id }) => {
         const now = new Date();
 
@@ -51,17 +95,21 @@ export default (io:Server, socket:Socket) => {
         socket.to(chat_id as string).emit("messageWasSeen", message_id);
     });
 
-    socket.on("enterRoom", async (chatId:string) => {
-        if (!chatId) {
-            return;
-        }
-        await socket.join(chatId);
+    socket.on("startWriting", async ({ user_id, username, chat_id }) => {
+        const chat = await Chats.findById(chat_id);
+        const receivers = chat?.chat_users.filter((receiver) => receiver.toString() !== user_id) ?? [];
+
+        receivers.forEach((receiverId) => {
+            socket.to(receiverId.toString()).emit("userIsWriting", username);
+        });
     });
 
-    socket.on("leaveRoom", async (chatId:string) => {
-        if (!chatId) {
-            return;
-        }
-        await socket.leave(chatId);
+    socket.on("stopWriting", async ({ user_id, username, chat_id }) => {
+        const chat = await Chats.findById(chat_id);
+        const receivers = chat?.chat_users.filter((receiver) => receiver.toString() !== user_id) ?? [];
+
+        receivers.forEach((receiverId) => {
+            socket.to(receiverId.toString()).emit("userStoppedWriting", username);
+        });
     });
 };
